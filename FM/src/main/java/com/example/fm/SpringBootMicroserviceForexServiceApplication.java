11601bb65f925fa9d28ec98019ce87ffac3cd9a5
OleksandRomaniuk <oleksandr.romaniuk@teamdev.com>
2022-10-09 18:08:17 +0300

    Add first microservice

---
+++ FM/src/main/java/com/example/fm/SpringBootMicroserviceForexServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.fm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class SpringBootMicroserviceForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceForexServiceApplication.class, args);
+	}
+}
